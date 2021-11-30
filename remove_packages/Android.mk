344265a436d559d1fe6fd72f6b1c1896fd5350ae
Gaganpreet Singh <gaganpannu83@gmail.com>
2021-11-30 17:20:42 +0530

    whyred: remove_packages: removed some not required prebuild packages Signed-off-by: IGaganpreetSingh <gaganpannu83@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,10 +4,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := arcore AmbientSensePrebuilt PrebuiltGmail PixelLiveWallpaperPrebuilt Chrome Chrome-Stub Drive GoogleFeedback GoogleCamera YouTube YouTubeMusicPrebuilt MaestroPrebuilt SafetyHubPrebuilt SprintDM SprintHM MyVerizonServices PartnerSetupPrebuilt
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt CarrierSetup Drive FM2 Maps MyVerizonServices OBDM_Permissions Showcase SprintDM SprintHM YouTube YouTubeMusicPrebuilt VZWAPNLib VzwOmaTrigger libqcomfm_jni obdm_stub qcom.fmradio Gmail Gallery2
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AndroidAutoStubPrebuilt AppDirectedSMSService Drive ConnMO DCMO
+LOCAL_OVERRIDES_PACKAGES += DMService GCS GoogleCamera MaestroPrebuilt Maps MicropaperPrebuilt MyVerizonServices
+LOCAL_OVERRIDES_PACKAGES += YouTube YouTubeMusicPrebuilt SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap
+LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM TurboPrebuilt Tycho USCCDM VZWAPNLib
+LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger OBDM_Permissions obdm_stub arcore DevicePolicyPrebuilt RecorderPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
 include $(BUILD_PREBUILT)
-
-#EmergencyInfoGoogleNoUi Flipendo
